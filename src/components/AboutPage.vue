c7bbb3d3b98b17e061329db29b163f6f72636721
摸鱼玩家 <power916@163.com>
2024-10-10 19:14:39 +0800

    🐛 fix: 修复移动端显示异常

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -72,9 +72,7 @@ fetchCommits();
 
 <style scoped>
 .modal {
-  width: 100%;
-  max-width: 500px;
-  min-width: 500px;
+  width: 500px;
   backdrop-filter: blur(5px);
   background-color: rgba(var(--background-color-rgb), 0.9);
   padding: 40px;
